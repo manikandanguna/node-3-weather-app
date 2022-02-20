63581f5316b56658e3de6a8d2d2f98397dee22ff
Manikandan <gmanikandan17@gmail.com>
2022-02-20 20:26:21 +0530

    add new tem in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,16 +2,16 @@ const request = require('request')
 const forecast = (longitude, latitude, callback) => {
     url = 'http://api.weatherstack.com/current?access_key=578dfd7310674e077815e9ef4c3c098a&query=' + latitude + ',' + longitude + '&units=f'
 
-    request({ url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('unable to connect weather server ! ', undefined)
         } else if (body.error) {
             callback('unable to find location ! ', undefined)
         }
         else {
-            callback(undefined,' it is currently '+body.current.temperature +' degreey and climate out there is ' + body.current.weather_descriptions[0]
+            callback(undefined, body.current.weather_descriptions[0] + ' it is currently ' + body.current.temperature + ' degree out. and its feel like ' + body.current.feelslike + ' degree out and the humidity is ' + body.current.humidity
             )
-            
+
         }
     })
 }
